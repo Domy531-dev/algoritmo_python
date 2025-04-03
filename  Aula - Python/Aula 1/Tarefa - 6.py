e4d51d0ffbe9a33f0b1ead3cfea437255bd70185
Domy531-dev <viniciussilva775.vs@gmail.com>
2025-04-02 21:01:49 -0300

    meu 6 commit

---
+++  Aula - Python/Aula 1/Tarefa - 6.py
@@ -0,0 +1,9 @@
+import math
+
+def calcular_area_circulo(raio):
+    return math.pi * (raio ** 2)
+
+# Exemplo de uso
+raio = float(input("Digite o raio do círculo: "))
+area = calcular_area_circulo(raio)
+print(f"A área do círculo com raio {raio} é {area:.2f}.")
