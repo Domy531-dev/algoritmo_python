8b35cc8f567147c544d46c595df4321eb5ffb77a
Domy531-dev <viniciussilva775.vs@gmail.com>
2025-04-04 21:35:14 -0300

    meu branch 2

---  Aula - Python/estruturadedecisao/Tarefa - 1.py
+++  Aula - Python/estruturadedecisao/Tarefa - 1.py
@@ -1,9 +1,9 @@
-numero1 = float(input("Digite o rpeimeimo número: "))
-numero2 = float(input("Digite o segundo número: "))
+numero1 = int(input("Digite o rpeimeimo número: "))
+numero2 = int(input("Digite o segundo número: "))
 
 if numero1 >  numero2:
-    print("O maior número é:", numero1)
-elif numero2 > numero1
+    print("O maior número é:", numero1 , "O menor número é:")
+elif numero2 == numero1
     print("O maior número é:", numero2)
 else:
     print("Os dois números são iguais.")
